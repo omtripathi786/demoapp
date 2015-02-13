3f5309ae865ef6b38cff0ee9bc9f88286d08ecdf
om tripathi <omtripathi88@gmail.com>
2015-02-14 01:23:15 +0530

    did some chnages in sms_cbv for login and registration

--- sms_cbv/urls.py
+++ sms_cbv/urls.py
@@ -1,11 +1,9 @@
 __author__ = 'Om Tripathi'
-
 from django.conf.urls import url, patterns
-
 from sms_cbv import views
-
 
 urlpatterns = patterns('',
                        url(r'^$', views.UserLogin.as_view()),
-                       #url(r'^sms/$', views.user_login)
+                       url(r'^register/$', views.UserRegistration.as_view()),
+
 )
--- sms_cbv/views.py
+++ sms_cbv/views.py
@@ -4,7 +4,11 @@ from django.views.generic import View
 from django.contrib.auth import authenticate
 from django.template import loader
 
+
 class UserLogin(View):
+    """
+        This is a class based view to handle login and authentication
+    """
     template_name = "sms_cbv/login.html"
 
     def get(self, request, *args, **kwargs):
@@ -12,18 +16,27 @@ class UserLogin(View):
         return HttpResponse(render(request, self.template_name, {'msg': msg}))
 
     def post(self, request, *args, **kwargs):
-        print 'check'
+        msg = ''
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username=username, password=password)
+        print user
         if user:
             if user.is_active:
                 msg = 'done'
             else:
                 msg = 'fail'
-
         return HttpResponse(render(request, self.template_name, {'msg': msg}))
 
 
+class UserRegistration(View):
+    """
+        This is a class based view to handle user registration
+    """
+    template_name = "sms_cbv/register.html"
+
+    def get(self, request, *args, **kwargs):
+        msg = ''
+        return HttpResponse(render(request, self.template_name, {'msg': msg}))
 
 
